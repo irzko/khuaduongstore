983e53e6031f3a5a68e4cfb3f698edec04dd314e
Kha <53520595+irzko@users.noreply.github.com>
2025-02-02 04:09:07 +0000

    .

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,10 +1,12 @@
 import { getAllProducts } from "@/lib/db";
 import SearchProductForm from "@/components/ui/search-product-form";
-
+import { Suspense } from "react";
 
 export default async function Page() {
   const products = await getAllProducts();
   return (
-    <SearchProductForm products={products} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchProductForm products={products} />
+    </Suspense>
   );
 }
--- components/ui/search-product-form.tsx
+++ components/ui/search-product-form.tsx
@@ -21,7 +21,7 @@ import fuzzy from "fuzzy";
 import { usePathname, useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 import { useDebouncedCallback } from "use-debounce";
-import { Suspense } from "react";
+
 function normalizeString(str: string): string {
   return str
     .toLowerCase()
@@ -121,50 +121,48 @@ export default function SearchProductForm({
           ]}
           gap="0.5rem"
         >
-          <Suspense fallback={<div>Loading...</div>}>
-            {filteredProducts.map((product) => (
-              <Card.Root key={product.id} asChild overflow="hidden">
-                <NextLink
-                  href={`/${slugify(product.name, {
-                    replacement: "-",
-                    remove: undefined,
-                    lower: true,
-                    strict: true,
-                    locale: "vi",
-                    trim: true,
-                  })}-${product.id}.html`}
+          {filteredProducts.map((product) => (
+            <Card.Root key={product.id} asChild overflow="hidden">
+              <NextLink
+                href={`/${slugify(product.name, {
+                  replacement: "-",
+                  remove: undefined,
+                  lower: true,
+                  strict: true,
+                  locale: "vi",
+                  trim: true,
+                })}-${product.id}.html`}
+              >
+                <Flex position="relative" aspectRatio={1}>
+                  <Image asChild alt={product.name}>
+                    <NextImage
+                      src={product.image.split("\n")[0] || "/no-image.jpg"}
+                      alt={product.name}
+                      style={{ objectFit: "cover" }}
+                      fill
+                      unoptimized
+                    />
+                  </Image>
+                </Flex>
+                <Card.Body
+                  gap="0.5rem"
+                  padding="0.5rem"
+                  direction="col"
+                  justifyContent="space-between"
                 >
-                  <Flex position="relative" aspectRatio={1}>
-                    <Image asChild alt={product.name}>
-                      <NextImage
-                        src={product.image.split("\n")[0] || "/no-image.jpg"}
-                        alt={product.name}
-                        style={{ objectFit: "cover" }}
-                        fill
-                        unoptimized
-                      />
-                    </Image>
-                  </Flex>
-                  <Card.Body
-                    gap="0.5rem"
-                    padding="0.5rem"
-                    direction="col"
-                    justifyContent="space-between"
-                  >
-                    <Heading lineClamp={2} size="md">
-                      {product.name}
-                    </Heading>
-                    <Text fontSize="md" color="red.500" fontWeight="bold">
-                      {Intl.NumberFormat("vi-VN", {
-                        style: "currency",
-                        currency: "VND",
-                      }).format(product.price)}
-                    </Text>
-                  </Card.Body>
-                </NextLink>
-              </Card.Root>
-            ))}
-          </Suspense>
+                  <Heading lineClamp={2} size="md">
+                    {product.name}
+                  </Heading>
+                  <Text fontSize="md" color="red.500" fontWeight="bold">
+                    {Intl.NumberFormat("vi-VN", {
+                      style: "currency",
+                      currency: "VND",
+                    }).format(product.price)}
+                  </Text>
+                </Card.Body>
+              </NextLink>
+            </Card.Root>
+          ))}
         </Grid>
       </Container>
     </>
