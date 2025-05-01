bbd6a5c15462623bd9aa854764c2f9b7aeab3370
Kha <53520595+irzko@users.noreply.github.com>
2025-05-01 09:41:11 +0000

    .

--- components/ui/carousel.tsx
+++ components/ui/carousel.tsx
@@ -1,45 +1,85 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Swiper as SwiperType } from "swiper";
+import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
 import NextImage from "next/image";
 import { Box, Image } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Carousel({ imageUrlList }: { imageUrlList: string[] }) {
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>();
   return (
-    <Box rounded="2xl" overflow="hidden" w="full" maxWidth="md" mx="auto">
-      <Swiper pagination={true} modules={[Pagination]}>
-        {imageUrlList.map((imageUrl, index) => (
-          <SwiperSlide key={index}>
-            <Box
-              position="relative"
-              w="full"
-              aspectRatio={{
-                base: 1 / 1,
-                md: 4 / 3,
-              }}
-            >
-              <Image
-                fit={{
-                  base: "cover",
-                  md: "contain",
+    <>
+      <Box rounded="2xl" overflow="hidden" w="full" maxWidth="md" mx="auto">
+        <Swiper
+          pagination={true}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Navigation, Thumbs]}
+        >
+          {imageUrlList.map((imageUrl, index) => (
+            <SwiperSlide key={index}>
+              <Box
+                position="relative"
+                w="full"
+                aspectRatio={{
+                  base: 1 / 1,
+                  md: 4 / 3,
                 }}
-                alt=""
-                asChild
               >
-                <NextImage
-                  src={imageUrl}
-                  fill
-                  unoptimized
+                <Image
+                  fit={{
+                    base: "cover",
+                    md: "contain",
+                  }}
                   alt=""
-                />
-              </Image>
-            </Box>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Box>
+                  asChild
+                >
+                  <NextImage src={imageUrl} fill unoptimized alt="" />
+                </Image>
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+      <Box overflow="hidden" w="full" maxWidth="md" mx="auto">
+        <Swiper
+          onSwiper={setThumbsSwiper}
+          spaceBetween={10}
+          slidesPerView={4}
+          freeMode={true}
+          watchSlidesProgress={true}
+          modules={[FreeMode, Navigation, Thumbs]}
+        >
+          {imageUrlList.map((imageUrl, index) => (
+            <SwiperSlide key={index}>
+              <Box
+                position="relative"
+                w="full"
+                rounded="lg"
+                overflow="hidden"
+                aspectRatio={{
+                  base: 1 / 1,
+                  md: 4 / 3,
+                }}
+              >
+                <Image
+                  fit={{
+                    base: "cover",
+                    md: "contain",
+                  }}
+                  alt=""
+                  asChild
+                >
+                  <NextImage src={imageUrl} fill unoptimized alt="" />
+                </Image>
+              </Box>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Box>
+    </>
   );
 }
