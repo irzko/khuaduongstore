626f05e506b62d18724719625c13ade855a6ee53
Kha <53520595+irzko@users.noreply.github.com>
2025-05-06 07:01:48 +0000

    .

--- app/tim-kiem/page.tsx
+++ app/tim-kiem/page.tsx
@@ -1,10 +1,10 @@
-import { getAllProducts } from "@/lib/db";
+import { getGroupedProducts } from "@/lib/db";
 import SearchProductForm from "@/components/ui/search-product-form";
 import { Suspense } from "react";
 import Fallback from "@/components/ui/fallback";
 
 export default async function Page() {
-  const products = await getAllProducts();
+  const products = await getGroupedProducts();
   return (
     <Suspense fallback={<Fallback />}>
       <SearchProductForm products={products} />
